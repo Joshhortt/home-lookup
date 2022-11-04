cb93612f90b7114d0b3cfa72ea89f19222cde7ee
Joshhortt <joshhortt@yahoo.com>
2022-11-04 19:55:34 +0000

    Create Auth Page, add Restricted Route-getServerSideProps

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,10 +1,22 @@
 // SIGN-UP & SIGN-IN PAGE
 
-// import Layout from "src/components/layout";
-// import FirebaseAuth from "src/components/firebaseAuth";
-// import { GetServerSideProps, NextApiRequest } from "next";
-// import { loadIdToken } from "src/auth/firebaseAdmin";
+import Layout from "src/components/layout";
+import FirebaseAuth from "src/components/firebaseAuth";
+import { GetServerSideProps, NextApiRequest } from "next";
+import { loadIdToken } from "src/auth/firebaseAdmin";
 
 export default function Auth() {
-  return <div>Auth</div>;
+  return <Layout main={<FirebaseAuth/> } />;
 }
+
+export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
+  const uid = await loadIdToken(req as NextApiRequest);
+
+  if (uid) {
+    res.setHeader("location", "/");
+    res.statusCode = 302;
+    res.end();
+  }
+
+  return { props: {} };
+};
